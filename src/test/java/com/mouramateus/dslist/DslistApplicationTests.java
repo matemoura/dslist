4da800e0cfc627b1d0e8db6de06056d0e52487d8
Mateus Moura <mateus51moura@gmail.com>
2023-07-31 22:15:40 -0300

    Project created

---
+++ src/test/java/com/mouramateus/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouramateus.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
